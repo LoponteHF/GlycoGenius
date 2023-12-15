7c5f515f9777acd96c6d487b3e93ffb85ad2bbf1
Hector Loponte <hectorfloponte@gmail.com>
2023-12-15 20:41:11 +0100

    Adjusted package structure - Working in local environment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Hector Franco Loponte',
     author_email='hectorfloponte@gmail.com',
-    description='GlycoGenius is a python script that aims to be an all-in-one solution for data analysis of glycomics data.',
+    description='GlycoGenius is an all-in-one solution for data analysis of glycomics data.',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -13,4 +13,6 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+    install_requres=["pandas", "scipy", "pyteomics",
+                     "dill", "numpy"],
 )
